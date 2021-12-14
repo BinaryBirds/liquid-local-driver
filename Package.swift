fdc8cf1f99a473ad9793d915d339656a079d9f62
Tibor Bödecs <mail.tib@gmail.com>
2021-12-14 14:35:34 +0100

    Dep updates

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "liquid-local-driver",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v12)
     ],
     products: [
         .library(name: "LiquidLocalDriver", targets: ["LiquidLocalDriver"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/binarybirds/liquid-kit.git", from: "1.3.1"),
+        .package(url: "https://github.com/binarybirds/liquid-kit.git", from: "1.3.2"),
     ],
     targets: [
         .target(name: "LiquidLocalDriver", dependencies: [
