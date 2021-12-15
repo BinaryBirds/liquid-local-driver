b068a1ab399d21f5d4b60eb4b0568fad4c398a59
Tibor Bödecs <mail.tib@gmail.com>
2021-12-15 08:53:10 +0100

    macOS 10.15 support

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "liquid-local-driver",
     platforms: [
-       .macOS(.v12)
+       .macOS(.v10_15)
     ],
     products: [
         .library(name: "LiquidLocalDriver", targets: ["LiquidLocalDriver"]),
